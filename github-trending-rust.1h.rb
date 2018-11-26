5a05ab2ee62563d91e35bba84af36037df4a60c5
Brett Mack <brett@brettmack.me>
2018-11-26 09:33:25 +0000

    Add descriptions to github trending

--- github-trending-rust.1h.rb
+++ github-trending-rust.1h.rb
@@ -35,7 +35,7 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
 doc.xpath('//ol[@class="repo-list"]/li').each do |node|
   node.xpath('.//h3/a').attribute('href').value.each_line do |s|
     s.slice!(0)
-    hash = { name: s, url: BASE_URL + s }
+    hash = { name: s, url: BASE_URL + s, stars: '?' }
 
     # api = 'https://api.github.com/repos/' + s
     # begin
@@ -48,15 +48,19 @@ doc.xpath('//ol[@class="repo-list"]/li').each do |node|
 
     # if code == '200'
     #   result = JSON.parse(res.read)
-    stars = "?"
+    # stars = "?"
     if node.xpath('./div[4]/span[3]').text.split("\n").count == 4 then
-    	stars = node.xpath('./div[4]/span[3]').text.split("\n")[2].split(' ')[0]
+    	hash[:stars] = node.xpath('./div[4]/span[3]').text.split("\n")[2].split(' ')[0]
     end
     #puts hash.fetch(:name)
     #puts node.xpath('./div[4]/span[3]').text.split("\n").count
-    puts hash.fetch(:name) + ' ⭐️ Daily: ' + stars + '| sizes=14 href=' + hash.fetch(:url)
+    # puts hash.fetch(:name) + ' ⭐️ Daily: ' + stars + '| sizes=14 href=' + hash.fetch(:url)
     # else
     #   puts "OMG!! #{code} #{message}"
     # end
   end
+  hash[:desc] = node.xpath('./div[3]/p').text.strip[0,70]
+  puts '---'
+  puts hash.fetch(:name) + ' ⭐️ Daily: ' + hash.fetch(:stars) + '| sizes=14 href=' + hash.fetch(:url)
+  puts hash.fetch(:desc)
 end
